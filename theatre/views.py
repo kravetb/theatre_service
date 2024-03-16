3cd72e3346b2421ffbb36b5ea25cd4fdfa5703d1
Your_Name Your_Surname <your@email>
2024-03-16 20:23:22 +0200

    Change problem with .env and with serializer.is_valid()

--- theatre/views.py
+++ theatre/views.py
@@ -100,11 +100,9 @@ class PlayViewSet(viewsets.ModelViewSet):
         movie = self.get_object()
         serializer = self.get_serializer(movie, data=request.data)
 
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @extend_schema(
         parameters=[
